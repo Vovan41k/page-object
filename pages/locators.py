c476d560d943aa1ed53c55cb4c8fde0ff93bf589
Vovan41k <fatuev_vladimir@mail.ru>
2024-12-22 01:21:10 +0300

    main_page methods removed to base_page

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,24 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
-class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
-    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
-class ProductPageLocators():
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div#messages div:nth-child(1)')
-    PRODUCT_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    PRODUCT_NAME = (By.CSS_SELECTOR, 'h1')
-    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
-    RESULT_PRODUCT_NAME = (By.CSS_SELECTOR, 'div.alertinner strong')
-    RESULT_PRODUCT_PRICE = (By.CSS_SELECTOR, 'div.alertinner p strong')
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div#messages div:nth-child(1)")
+    PRODUCT_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    RESULT_PRODUCT_NAME = (By.CSS_SELECTOR, "div.alertinner strong")
+    RESULT_PRODUCT_PRICE = (By.CSS_SELECTOR, "div.alertinner p strong")
--- test_product_page.py
+++ test_product_page.py
@@ -20,28 +20,40 @@ import time
 #     product_page.should_be_similar_price()
 
 
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
-    page = MainPage(browser, link)
-    page.open()
-    product_page = ProductPage(browser, browser.current_url, timeout=0)
-    product_page.add_product_to_basket()
-    product_page.should_not_be_success_message()
+# @pytest.mark.xfail
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
+#     page = MainPage(browser, link)
+#     page.open()
+#     product_page = ProductPage(browser, browser.current_url, timeout=0)
+#     product_page.add_product_to_basket()
+#     product_page.should_not_be_success_message()
 
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
-    page = MainPage(browser, link)
-    page.open()
-    product_page = ProductPage(browser, browser.current_url, timeout=0)
-    product_page.should_not_be_success_message()
+# def test_guest_cant_see_success_message(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
+#     page = MainPage(browser, link)
+#     page.open()
+#     product_page = ProductPage(browser, browser.current_url, timeout=0)
+#     product_page.should_not_be_success_message()
 
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2'
-    page = MainPage(browser, link)
+# @pytest.mark.xfail
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2'
+#     page = MainPage(browser, link)
+#     page.open()
+#     product_page = ProductPage(browser, browser.current_url, timeout=0)
+#     product_page.add_product_to_basket()
+#     product_page.should_not_be_success_message()
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    product_page = ProductPage(browser, browser.current_url, timeout=0)
-    product_page.add_product_to_basket()
-    product_page.should_not_be_success_message()
+    page.should_be_login_link()
+    
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
